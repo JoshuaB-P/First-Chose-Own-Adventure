392795a1358961f151fe7880142113aaca4faae7
theCoderSchool <tcsbcoach@gmail.com>
2017-10-14 12:05:52 -0700

    second draft

--- Joshua-Bernard-Pearl/middle_and_atk.py
+++ Joshua-Bernard-Pearl/middle_and_atk.py
@@ -1,4 +1,3 @@
-
 # def middle(x,y,z):
 # 	nums = [x,y,z]
 # 	list.sort(nums)
@@ -32,20 +31,34 @@
 
 import random
 
-class otha:
-	stre = 5
-	weapon = 5
-	defe = 5
-	spd = 4
-	hp = 10
-class blob:
-	__init__():
+class Player:
+	def __init__(self):
+		self.name = Jose
+		self.stre = 5
+		self.weapn_name = "sword"
+		self.weapon = 5
+		self.defe = 5
+		self.spd = 4
+		self.hp = 10
+class Blob:
+	def __init__(self):
+		self.name = "Blob"
 		self.stre = random.randint(3,5)
+		self.weapon_name = "body"
 		self.weapon = 1
 		self.defe = random.randint(3,5)
 		self.spd = random.randint(1,2)
 		self.hp = 10
-
+class Custom_Enemy:
+	def __init__(self,name,level,stre,weapon_name,weapon,defe,spd,hp):
+		self.name = name
+		self.level = level
+		self.stre = stre
+		self.weapon_name = weapon_name
+		self.weapon = weapon
+		self.defe = defe
+		self.spd = spd
+		self.hp = hp
 
 
 # def dam_value(x,y):
@@ -54,7 +67,21 @@ class blob:
 
 
 def combat(x,y):
+	print(x.name+":\nStrength: "+x.stre+"\nWeapon: "+x.weapon_name+"("+x.weapon+")\nDefence: "+x.defe+"\nSpeed: "+x.spd+"HP: "+x.hp)
+	print(y.name+":\nStrength: "+y.stre+"\nWeapon: "+y.weapon_name+"("+y.weapon+")\nDefence: "+y.defe+"\nSpeed: "+y.spd+"HP: "+y.hp)
 	answer = input("Type atk to atack\nType run to try and run away")
 	if answer == "atk":
-		print("oponent HP = "hp-(x.stre*x.weapon//y.defe)
+		if x.spd > y.spd:
+		print("oponent HP = "+str(y.hp-(x.stre*x.weapon//y.defe))
+		print("Your HP = "+str(x.hp-(y.stre*y.weapon//x.defe))
+		else:
+		print("Your HP = "+str(x.hp-(y.stre*y.weapon//x.defe))
+		print("oponent HP = "+str(y.hp-(x.stre*x.weapon//y.defe))
+
+
+jose = Player()
+
+blob = Blob()
+
+combat(jose,blob)
 
